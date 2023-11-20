a4be03c098ea29f8dff1cead0080f4bf13e90b2d
Joel-Ax <joelaxelsson97@gmail.com>
2023-11-20 11:43:17 +0100

    Added the weather fetch function, trying to debug why it's not working

--- dist/index.js
+++ dist/index.js
@@ -20,3 +20,27 @@ function getLocationAPI(api) {
         (document.querySelector("#userLocation").innerHTML = data.locality)
     );
 }
+
+function loadLoaction() {
+  let test = document.getElementsByName("input").value;
+  Node.addEvenListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      loadWeatherAPI(test);
+    }
+  });
+}
+
+function loadWeatherAPI(location) {
+  let link =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&units=metric&daily&appid=1f4819738a1f7aaf2e1456e4866ae602";
+
+  fetch(link)
+    .then((res) => res.json())
+    .then((weather) => {
+      //Where you generate the data
+    });
+}
+
+loadWeatherAPI(Stockholm);
